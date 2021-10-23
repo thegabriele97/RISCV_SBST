2a753a59583108a27390b228738a6a4da55091c4
LA GRECA SALVATORE GABRIELE <s281589@po.polito.it>
2021-10-23 18:23:31 +0200

    added riscv lab

--- lab2/tmuxall.tcl
+++ lab2/tmuxall.tcl
@@ -5,14 +5,14 @@ read_netlist c6288.vhd -master
 run_build_model c6288
 # run_build_model b10
 
-add_po_masks -all
+# add_po_masks -all
 # remove_po_masks datao(1)
 # remove_po_masks datao(2)
 # remove_po_masks datao(3)
 
-# add_po_masks datao(1)
-# add_po_masks datao(2)
-# add_po_masks datao(3)
+add_po_masks datao(1)
+add_po_masks datao(2)
+add_po_masks datao(3)
 # report_primitives -pos
 
 # add_pi_constraints x datai(0)
@@ -25,14 +25,14 @@ run_drc c6288.stil
 set_faults -model stuck
 add_faults -all
 
-# set_patterns -external c6288.stil
-# run_simulation
-# run_fault_sim
+set_patterns -external c6288.stil
+run_simulation
+run_fault_sim
 
-set_patterns -internal
-run_atpg -auto_compression
+# set_patterns -internal
+# run_atpg -auto_compression
 
 set_faults -summary verbose -fault_coverage
 report_summaries
 
-exit
+# exit
