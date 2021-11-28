60f22cbe0f82d9a548356c179c5eaed706aad65a
BATTILANA MATTEO <s281389@po.polito.it>
2021-11-28 21:45:23 +0100

    added first implementation march la - to be tested

--- ASSIGNMENT/sbst/main.c
+++ ASSIGNMENT/sbst/main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int d;
-
 int main(int argc, char *argv[])
 {
-	int a[5] = { 1323, 234, 43, 545343, 1000 };
-	int b[5] = { 13, 2, 4, 54, 10 };
-	volatile int c[5];
+	// int a[5] = { 1323, 234, 43, 545343, 1000 };
+	// int b[5] = { 13, 2, 4, 54, 10 };
+	// volatile int c[5];
 
-	int i;
+	// int i;
 
-	for(i=0; i<5; i++) {
-		c[i] = a[i] / b[i];
-	}
+	// for(i=0; i<5; i++) {
+	// 	c[i] = a[i] / b[i];
+	// }
 
 
     /* inline assembly */
