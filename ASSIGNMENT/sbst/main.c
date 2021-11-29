9a0b073a4642ef38491ff955dbb228d02ad590de
BATTILANA MATTEO <s281389@po.polito.it>
2021-11-29 18:02:43 +0100

    halt error

--- ASSIGNMENT/sbst/main.c
+++ ASSIGNMENT/sbst/main.c
@@ -3,11 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-	// int a[5] = { 1323, 234, 43, 545343, 1000 };
-	// int b[5] = { 13, 2, 4, 54, 10 };
-	// volatile int c[5];
+	int a[5] = { 1323, 234, 43, 545343, 1000 };
+	int b[5] = { 13, 2, 4, 54, 10 };
+	volatile int c[5];
 
-	// int i;
+	int i;
 
 	// for(i=0; i<5; i++) {
 	// 	c[i] = a[i] / b[i];
@@ -18,5 +18,10 @@ int main(int argc, char *argv[])
     //asm volatile("ecall");
     /* write something to stdout */
     /* printf("hello mondo world!\n"); */
+	while(1) {
+			for(i=0; i<5; i++) {
+		c[i] = a[i] / b[i];
+	}
+	}
     return EXIT_SUCCESS;
 }
